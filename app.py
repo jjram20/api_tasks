02a70fefd55cc3e298407c6f635eec4b815f1923
jjram20 <jde.ram9@gmail.com>
2025-02-14 15:12:43 -0600

    JWT implemented for users routes

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 from utils.db import db
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///project.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'secret_key'
 
 db.init_app(app)
 
 with app.app_context():
+    jwt = JWTManager(app)
+    
     from routes.users_route import users_bp
     from routes.tasks_route import tasks_bp
 
--- routes/users_route.py
+++ routes/users_route.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, jsonify, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+import datetime
 from utils.db import db
 from models.user import User
 
@@ -7,36 +10,64 @@ users_bp = Blueprint('users', __name__)
 @users_bp.route("/new_user", methods=['POST'])
 def new_user():
     req = request.get_json(force=True)
-    print(req)
-    user = req['user']
-    password = req['password']
-    new_user = User(user, password)
-    db.session.add(new_user)
-    db.session.commit()
-    return "New user registered"
+    user = req.get('user')
+    password = req.get('password')
+    if user and password:
+        hash_password = generate_password_hash(password)
+        new_user = User(user, hash_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return "New user registered"
+    else:
+        return "Data required is not complete"
+
+@users_bp.route("/login_user", methods=['POST'])
+def login_user():
+    req = request.get_json(force=True)
+    user = req.get('user')
+    user_db = User.query.filter_by(user=user).first()
+    password = req.get('password')
+    if user and check_password_hash(user_db.password, password):
+        access_token = create_access_token(identity=user, expires_delta=datetime.timedelta(hours=1))
+        return jsonify(access_token=access_token), 200
+    return "Invalid credentials"
 
 @users_bp.route("/get_users", methods=['GET'])
+@jwt_required()
 def get_users():
+    current_user = get_jwt_identity()
+    print(current_user)
     users = User.query.all()
     users_data = [user.to_dict() for user in users]
     response = {"status": 200, "users": users_data}
     return response
 
 @users_bp.route("/edit_user/<id>", methods=['PUT'])
+@jwt_required()
 def edit_user(id):
+    current_user = get_jwt_identity()
     user = User.query.get(id)
     req = request.get_json(force=True)
-    if req['new_user']:
-        new_user = req['new_user']
-        user.user = new_user
-    if req['new_password']:
-        new_password = req['new_password']
-        user.password = new_password
+    print(req)
+    if req.get("user"):
+        print("Nuevo usuario")
+        user.user = req.get('user')
+    if req.get('password'):
+        print("Nuevo password")
+        new_password = req.get('password')
+        hash_new_password = generate_password_hash(new_password)
+        user.password = hash_new_password
+    print("USER")
+    print(user)
+    print(user.user)
+    print(user.password)
     db.session.commit()
     return "User updated"
 
 @users_bp.route("/delete_user/<id>", methods=['DELETE'])
+@jwt_required()
 def delete_user(id):
+    current_user = get_jwt_identity()
     user = User.query.get(id)
     db.session.delete(user)
     db.session.commit()
